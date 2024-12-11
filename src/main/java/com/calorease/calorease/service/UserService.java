2ea7f0f0f03ebe4994c98dc127c9ad0322a3a7a7
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-11 18:52:22 -0500

    Add UserService class and its logic

---
+++ src/main/java/com/calorease/calorease/service/UserService.java
@@ -0,0 +1,32 @@
+package com.calorease.calorease.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.calorease.calorease.repository.UserRepository;
+import java.util.Optional;
+import java.util.List;
+import com.calorease.calorease.entity.User;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+    
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email)
+        		.orElseThrow(() -> new RuntimeException("User not found with email: " + email));
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
