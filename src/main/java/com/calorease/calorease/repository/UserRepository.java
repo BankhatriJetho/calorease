cbe2ccf23af4e7333b9b7a5ea68bab47a2398fb6
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-11 18:51:24 -0500

    Add UserRepository class and its logic

---
+++ src/main/java/com/calorease/calorease/repository/UserRepository.java
@@ -0,0 +1,13 @@
+
+package com.calorease.calorease.repository;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.calorease.calorease.entity.*;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+}
