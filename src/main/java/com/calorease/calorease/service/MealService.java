3f220cb439382f2b2b9dd930da0ee1aa2bca5a13
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-20 18:01:57 -0500

    Added and tested Meal update endpoint, pagination and filtering

--- src/main/java/com/calorease/calorease/service/MealService.java
+++ src/main/java/com/calorease/calorease/service/MealService.java
@@ -1,8 +1,14 @@
 package com.calorease.calorease.service;
 
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+
+import com.calorease.calorease.dto.MealDTO;
 import com.calorease.calorease.entity.Meal;
 import com.calorease.calorease.entity.User;
 import com.calorease.calorease.repository.MealRepository;
@@ -36,6 +42,24 @@ public class MealService {
         }
         mealRepository.deleteById(id);
     }
+    
+    public Meal updateMeal(Integer mealId, MealDTO mealDTO) {
+    	return mealRepository.findById(mealId)
+                .map(existingMeal -> {
+                    existingMeal.setName(mealDTO.getName());
+                    existingMeal.setCalories(mealDTO.getCalories());
+                    existingMeal.setDate(mealDTO.getDate());
+                    return mealRepository.save(existingMeal);
+                })
+                .orElseThrow(() -> new RuntimeException("Meal not found with ID: " + mealId));
+    	
+    }
+    
+    public Page<MealDTO> getFilteredMeals(Integer userId, Integer minCalories, Integer maxCalories, LocalDate date, int page, int size) {
+    	Pageable pageable = PageRequest.of(page, size);
+    	Page<Meal> meals = mealRepository.findFilteredMeals(userId, minCalories, maxCalories, date, pageable);
 
-
+    	// Map to DTO
+    	return meals.map(MealDTO::from);
+    }
 }
