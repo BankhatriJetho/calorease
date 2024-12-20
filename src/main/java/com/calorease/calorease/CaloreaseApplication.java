c8856992d2d82431e326b4a41b0ed6e35197872c
BankhatriJetho <bankhatrijetho524@gmail.com>
2024-12-20 16:43:09 -0500

    Added base package path

--- src/main/java/com/calorease/calorease/CaloreaseApplication.java
+++ src/main/java/com/calorease/calorease/CaloreaseApplication.java
@@ -3,7 +3,7 @@ package com.calorease.calorease;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.calorease")
 public class CaloreaseApplication {
 
 	public static void main(String[] args) {
