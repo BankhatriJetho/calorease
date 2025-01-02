16bde90225c69569f22bd5341b1d64e77916a45e
BankhatriJetho <bankhatrijetho524@gmail.com>
2025-01-02 17:52:23 -0500

    User registration endpoint tested

--- src/main/java/com/calorease/calorease/service/UserService.java
+++ src/main/java/com/calorease/calorease/service/UserService.java
@@ -2,12 +2,16 @@ package com.calorease.calorease.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.calorease.calorease.repository.UserRepository;
 
 import jakarta.transaction.Transactional;
 
 import java.util.Optional;
 import java.util.List;
+
+import com.calorease.calorease.dto.UserRegistrationDTO;
 import com.calorease.calorease.entity.User;
 
 @Service
@@ -15,14 +19,36 @@ import com.calorease.calorease.entity.User;
 public class UserService {
     
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
     
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    public User registerUser(UserRegistrationDTO userDTO) {
+        // Check if the email is already in use
+        if (userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
+            throw new RuntimeException("Email is already in use.");
+        }
+
+        // Map DTO to Entity
+        User user = new User();
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        // Persist user
+        return userRepository.save(user);
+    }
+
+    // Register user without password (UserDTO)
     public User registerUser(User user) {
-    	System.out.println("Registering user: " + user);
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            throw new RuntimeException("Email is already in use.");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
